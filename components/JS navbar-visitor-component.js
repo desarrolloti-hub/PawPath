8c4fe3dee58bff16cfa4be5631dbae67f9e85914
Emanuel Campa <desarrolloti@rhafasoluciones.com>
2026-03-10 10:12:46 -0600

    creacion de mapa

--- components/JS navbar-visitor-component.js
+++ components/JS navbar-visitor-component.js
@@ -415,7 +415,7 @@
             <ul class="paw-nav-links">
                 <li><a href="/">Inicio</a></li>
                 <li><a href="/user/visitor/foro/Foro.html">Foro</a></li>
-                <li><a href="#map">Mapa</a></li>
+                <li><a href="/user/visitor/MapaForo/mapaforo.html">Mapa</a></li>
                 <li><a href="/user/visitor/citas/Citas.html">Agendar Cita</a></li>
                 <li><a href="#planes">Planes</a></li>
             </ul>
